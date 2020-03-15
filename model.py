2b7a7b3a5b5065d2661f5ac038fbf1de8f1f62e5
WuQiqi <abc_577@qq.com>
2020-03-15 20:40:44 +0800

    修复bug

--- model.py
+++ model.py
@@ -9,6 +9,8 @@ from DataSet import DataSet
 BATCH_SIZE = 128
 SHUFFLE_BUFFER_SIZE = 1000
 learning_rate = 0.001
+EPOCHS = 10
+
 
 ds = DataSet()
 train_dataset, test_dataset = ds.load()
@@ -32,29 +34,31 @@ model.compile(loss='mse', optimizer=tf.keras.optimizers.RMSprop(learning_rate), 
 print(model.summary())
 
 
-history = model.fit(train_dataset, epochs=50)
+history = model.fit(train_dataset, epochs=EPOCHS, validation_split=0.2, verbose=0)
 
 
-# def plot_history(history):
-#     hist = pd.DataFrame(history.history)
-#     hist['epoch'] = history.epoch
-#
-#     plt.figure()
-#     plt.xlabel('Epoch')
-#     plt.ylabel('Mean Abs Error [MPG]')
-#     plt.plot(hist['epoch'], hist['mae'], label='Train Error')
-#     plt.plot(hist['epoch'], hist['val_mae'], label='Val Error')
-#     # plt.ylim([0, 5])
-#     plt.legend()
-#
-#     plt.figure()
-#     plt.xlabel('Epoch')
-#     plt.ylabel('Mean Square Error [$MPG^2$]')
-#     plt.plot(hist['epoch'], hist['mse'], label='Train Error')
-#     plt.plot(hist['epoch'], hist['val_mse'], label='Val Error')
-#     # plt.ylim([0, 20])
-#     plt.legend()
-#     plt.savefig('./history.png')
+def plot_history(history):
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Abs Error [MPG]')
+    plt.plot(hist['epoch'], hist['mae'], label='Train Error')
+    # plt.plot(hist['epoch'], hist['val_mae'], label='Val Error')
+    # plt.ylim([0, 5])
+    plt.legend()
+    plt.savefig('./mae.png')
+    plt.close()
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Square Error [$MPG^2$]')
+    plt.plot(hist['epoch'], hist['mse'], label='Train Error')
+    # plt.plot(hist['epoch'], hist['val_mse'], label='Val Error')
+    # plt.ylim([0, 20])
+    plt.legend()
+    plt.savefig('./mse.png')
+    plt.close()
 
 
 # plot_history(history)
@@ -63,8 +67,9 @@ hist['epoch'] = history.epoch
 print(hist.tail())
 
 model.evaluate(test_dataset)
+
 path = './model/model_{}.h5'.format(time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time())))
-if not os.path.exists(path):
-    os.makedirs(path)
+# if not os.path.exists(path):
+#     os.makedirs(path)
 
 model.save(path)
