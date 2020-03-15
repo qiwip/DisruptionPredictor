39c822c4bcaef387f3b73edd1fd723133ebd772e
WuQiqi <abc_577@qq.com>
2020-03-15 20:15:38 +0800

    修复bug

--- model.py
+++ model.py
@@ -35,29 +35,33 @@ print(model.summary())
 history = model.fit(train_dataset, epochs=50)
 
 
-def plot_history(history):
-    hist = pd.DataFrame(history.history)
-    hist['epoch'] = history.epoch
+# def plot_history(history):
+#     hist = pd.DataFrame(history.history)
+#     hist['epoch'] = history.epoch
+#
+#     plt.figure()
+#     plt.xlabel('Epoch')
+#     plt.ylabel('Mean Abs Error [MPG]')
+#     plt.plot(hist['epoch'], hist['mae'], label='Train Error')
+#     plt.plot(hist['epoch'], hist['val_mae'], label='Val Error')
+#     # plt.ylim([0, 5])
+#     plt.legend()
+#
+#     plt.figure()
+#     plt.xlabel('Epoch')
+#     plt.ylabel('Mean Square Error [$MPG^2$]')
+#     plt.plot(hist['epoch'], hist['mse'], label='Train Error')
+#     plt.plot(hist['epoch'], hist['val_mse'], label='Val Error')
+#     # plt.ylim([0, 20])
+#     plt.legend()
+#     plt.savefig('./history.png')
 
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Abs Error [MPG]')
-    plt.plot(hist['epoch'], hist['mae'], label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mae'], label='Val Error')
-    # plt.ylim([0, 5])
-    plt.legend()
 
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Square Error [$MPG^2$]')
-    plt.plot(hist['epoch'], hist['mse'], label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mse'], label='Val Error')
-    # plt.ylim([0, 20])
-    plt.legend()
-    plt.savefig('./history.png')
+# plot_history(history)
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+print(hist.tail())
 
-
-plot_history(history)
 model.evaluate(test_dataset)
 path = './model/model_{}.h5'.format(time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time())))
 if not os.path.exists(path):
