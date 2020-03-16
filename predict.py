63f6c8111823e4be53dba487f179190592fff3f0
WuQiqi <abc_577@qq.com>
2020-03-16 19:37:15 +0800

    修改模型输出预测

--- predict.py
+++ predict.py
@@ -1,11 +1,13 @@
 import os
+import sys
 import tensorflow as tf
 from CreateDataFrame import Cutter
 import matplotlib.pyplot as plt
 
 
+shot = int(sys.argv[1])
 cutter = Cutter(normalized=True)
-x, y = cutter.get_one(1064320)
+x, y = cutter.get_one(shot)
 
 model = tf.keras.models.load_model(os.path.join('model', 'main', 'model.h5'))
 
@@ -15,4 +17,4 @@ y_ = model.predict(x)
 plt.figure()
 plt.plot(y)
 plt.plot(y_)
-plt.savefig('result.png')
+plt.savefig(os.path.join('model', 'main', 'result_{}.png'.format(shot)))
