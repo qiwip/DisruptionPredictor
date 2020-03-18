b28d377caf9218213a9915ce42765b52dc3675b0
WuQiqi <abc_577@qq.com>
2020-03-18 14:46:26 +0800

    修改预测输出bug

--- predict.py
+++ predict.py
@@ -9,14 +9,15 @@ shot = int(sys.argv[1])
 cutter = Cutter(normalized=True)
 x, y = cutter.get_one(shot)
 
-# dataset = tf.data.Dataset.from_tensor_slices((x, y))
-# model = tf.keras.models.load_model(os.path.join('model', 'main', 'model.h5'))
-#
-# print(model.summary())
-# y_ = model.predict(x)
-# model.evaluate(dataset)
+dataset = tf.data.Dataset.from_tensor_slices((x, y))
+model = tf.keras.models.load_model(os.path.join('model', 'main', 'model.h5'))
+model.evaluate(dataset)
+
+print(model.summary())
+y_ = model.predict(x)
+
 plt.figure()
 plt.plot(y, label='y')
-# plt.plot(y_, label='y_predict')
-# plt.legend()
+plt.plot(y_, label='y_predict')
+plt.legend()
 plt.savefig(os.path.join('model', 'main', 'result_{}.png'.format(shot)))
