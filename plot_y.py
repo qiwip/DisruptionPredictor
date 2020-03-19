bc378b99ea18469588c1d8a21857bafeddffb3f6
WuQiqi <abc_577@qq.com>
2020-03-19 22:05:36 +0800

    修复bug

--- plot_y.py
+++ plot_y.py
@@ -6,9 +6,18 @@ import matplotlib.pyplot as plt
 path_npy = os.path.join('model', 'result')
 path_pic = os.path.join('model', 'pic')
 
+shots = dict()
+with open(os.path.join('log', 'ShotsInDataset.txt'), 'r') as f:
+    for i in f.readlines():
+        shots[(i.split(' ')[0])] = (i.split(' ')[1], i.split(' ')[2].replace('\n', ''))
+
 if not os.path.exists(path_pic):
-    os.makedirs(path_pic)
+    os.makedirs(os.path.join(path_pic, 'd'))
+    os.makedirs(os.path.join(path_pic, 'u'))
+
 for file in os.listdir(path_npy):
+    if shots[file.replace('.npy', '').replace('y_y_', '')][1] == 'u':
+        continue
     data = np.load(os.path.join(path_npy, file))
     y = data[0]
     y_ = data[1]
@@ -17,7 +26,6 @@ for file in os.listdir(path_npy):
     plt.plot(y, label='y')
     plt.plot(y_, label='y_predict')
     plt.legend()
-    plt.savefig(os.path.join(path_pic, file.replace('npy', 'png')))
-    # plt.show()
+    plt.savefig(os.path.join(path_pic, shots[file.replace('.npy', '').replace('y_y_', '')][1],
+                             file.replace('npy', 'png')))
     plt.close()
-
